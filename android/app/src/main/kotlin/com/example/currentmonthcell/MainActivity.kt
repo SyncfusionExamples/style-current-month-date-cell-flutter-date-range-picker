83bfa1819f60f4cc93daeec92515dd380a6e1bd3
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-17 18:13:09 +0530

    FLUT-3195 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/currentmonthcell/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.currentmonthcell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(CurrentMonthCellCustomization());
+
+class CurrentMonthCellCustomization extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+            child: SfDateRangePicker(
+              view: DateRangePickerView.month,
+              monthViewSettings: DateRangePickerMonthViewSettings(showTrailingAndLeadingDates: true,
+              ),
+              monthCellStyle: DateRangePickerMonthCellStyle(
+                todayTextStyle: TextStyle(color: Colors.cyanAccent),
+                  cellDecoration: BoxDecoration(color:Colors.teal),),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
